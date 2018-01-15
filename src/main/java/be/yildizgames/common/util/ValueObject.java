ae8289043664933ac30bb83b3a806a2a773d9092
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2018-01-15 20:03:37 +0100

    [YCU-0] Add value object.

---
+++ src/main/java/be/yildizgames/common/util/ValueObject.java
@@ -0,0 +1,31 @@
+package be.yildiz.common;
+
+/**
+ * @author Grégory Van den Borre
+ */
+public class ValueObject {
+
+    /**
+     * Value.
+     */
+    public final int value;
+
+    protected ValueObject(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        return this == o || !(o == null || getClass() != o.getClass()) && value == ((ValueObject) o).value;
+    }
+
+    @Override
+    public final int hashCode() {
+        return value;
+    }
+
+    @Override
+    public final String toString() {
+        return String.valueOf(this.value);
+    }
+}
