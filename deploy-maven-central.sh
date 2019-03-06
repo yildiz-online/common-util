bc75642897ac7af8c0cbf8fd6afa0a5397925931
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-03-06 12:46:56 +0100

    Update deploy-maven-central.sh

--- deploy-maven-central.sh
+++ deploy-maven-central.sh
@@ -4,7 +4,7 @@ echo "Building $BRANCH branch"
 
 SECRETS=$(curl -sS -H "X-Vault-Token: $VAULT_TOKEN" -X GET https://vault.yildiz-games.be/v1/secret/yildiz-engine)
 
-echo $SECRETS | jq -r '.data'
+echo $SECRETS | jq -r '.data.SONAR_ORGANIZATION'
 
 #GH_TOKEN=$SECRETS | jq -r '.data.GH_TOKEN'
 #GPG_KEY=$SECRETS | jq -r '.data.GPG_KEY'
