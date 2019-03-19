9404ca94546f5d5f5ccc8953a4b3edd70ee395e4
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-03-19 07:03:54 +0100

    [WIP] Rollback prevault.

--- deploy-maven-central.sh
+++ deploy-maven-central.sh
@@ -4,17 +4,6 @@ echo "Building $BRANCH branch"
 
 SECRETS=$(curl -sS -H "X-Vault-Token: $VAULT_TOKEN" -X GET https://vault.yildiz-games.be/v1/secret/yildiz-engine)
 
-GH_TOKEN=$(echo $SECRETS | jq -r '.data.GH_TOKEN')
-GPG_KEY=$(echo $SECRETS | jq -r '.data.GPG_KEY')
-#GPG_PWD=$(echo $SECRETS | jq -r '.data.GPG_PWD')
-OPENSSL_PWD=$(echo $SECRETS | jq -r '.data.OPENSSL_PWD')
-#OSSRH_PWD_TOKEN=$(echo $SECRETS | jq -r '.data.OSSRH_PWD_TOKEN')
-#OSSRH_USER_TOKEN=$(echo $SECRETS | jq -r '.data.OSSRH_USER_TOKEN')
-REPO_PASSWORD=$(echo $SECRETS | jq -r '.data.REPO_PASSWORD')
-REPO_USER=$(echo $SECRETS | jq -r '.data.REPO_USER')
-SONAR=$(echo $SECRETS | jq -r '.data.SONAR')
-SONAR_ORGANIZATION=$(echo $SECRETS | jq -r '.data.SONAR_ORGANIZATION')
-
 if [ "$BRANCH" = "develop" ]; then
   openssl version -a
   openssl aes-256-cbc -pass pass:$OPENSSL_PWD -in ../build-resources/private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s ../build-resources/settings.xml org.jacoco:jacoco-maven-plugin:prepare-agent clean deploy sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=$SONAR_ORGANIZATION -Dsonar.login=$SONAR
